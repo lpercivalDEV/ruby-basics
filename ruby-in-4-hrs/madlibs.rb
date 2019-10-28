9b160bd13fbc7ffd621bbcad331498d14894b4df
lpercivalDEV <lpercival.dev@gmail.com>
2019-10-28 16:40:33 -0400

    Create madlibs app that allows users to plug inputs into texts

---
+++ ruby-in-4-hrs/madlibs.rb
@@ -0,0 +1,12 @@
+puts "Enter a food: "
+food = gets.chomp()
+
+puts "Enter an adverb: "
+adverb = gets.chomp()
+
+puts "Enter a noun: "
+noun = gets.chomp()
+
+puts ("Winner winner " + food + " dinner!")
+puts ("The sky is " + adverb)
+puts ("Hello, welcome to the end of the " + noun)
